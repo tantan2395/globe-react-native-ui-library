4e88ca708cc34f2bb214472fcff544c5a88c1828
Jonathan Cantora <tantancantora@gmail.com>
2024-04-18 18:23:49 +0800

    feat: added ProgressBar

---
+++ src/progressBars/ProgressBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useProgress } from '../provider/progressContext';
+
+const ProgressBar: React.FC = () => {
+  const { totalSteps, completedSteps } = useProgress();
+  const progressPercentage = (completedSteps / totalSteps) * 100;
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.progressBar, { width: `${progressPercentage}%` }]} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 4,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 5,
+    overflow: 'hidden',
+  },
+  progressBar: {
+    height: '100%',
+    backgroundColor: '#2196f3',
+  },
+});
+
+export default ProgressBar;
