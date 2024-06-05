a212ddfe1d02795dd831446b47647f214635d859
Sheen Fabile <sheen.fabile@globe.com.ph>
2024-06-05 12:16:50 +0800

    chore: added props to handle dynamic showing of info content and sub content

--- src/v1/icons/InformationIconText.tsx
+++ src/v1/icons/InformationIconText.tsx
@@ -1,32 +1,97 @@
-import { Image, StyleSheet, View, type ViewStyle } from 'react-native';
-import SecondaryText from '../labels/secondaryText';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  type TextStyle,
+  type ViewStyle,
+} from 'react-native';
 import React from 'react';
+import { SecondaryText } from 'globe-react-native-ui-library';
 
 /**
  * Information Icon Component
  *
  * @param iconSrc - The source of the icon to be displayed. This can be a local image asset or a remote URL (required).
  * @param infoContent - The text content providing information related to the icon (required).
+ * @param infoSubContent - The text sub content providing additional information related to the icon (optional).
  * @param style - Optional styles to be applied to the container of the icon and informational text. Allows for customization of the component's appearance (optional).
+ * @param highlightedText = Array of string to be highlighted (optional).
+ * @param highlightedTextStyle = Additional Styling to the hightlighted text (optional).
  * @returns
  */
 interface InformationIconProps {
   iconSrc: any;
   infoContent: string;
+  infoSubContent?: string;
   style?: ViewStyle;
+  highlightedText?: string[];
+  highlightedTextStyle?: TextStyle;
   testId?: string;
 }
 
 const InformationIconText: React.FC<InformationIconProps> = ({
   iconSrc,
   infoContent,
+  infoSubContent,
   style,
+  highlightedText,
+  highlightedTextStyle,
   testId = 'information-container',
 }) => {
+  function highlightPhrases(input: string, phrases: string[]) {
+    if (!phrases || phrases.length === 0) {
+      return [{ text: input, highlight: false }];
+    }
+
+    // Escape special characters in the phrases
+    const escapeStringForRegExp = (str: string) => {
+      return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // Escape special characters
+    };
+
+    // Create a pattern to match any of the phrases
+    const pattern = phrases.map(escapeStringForRegExp).join('|');
+    const regexp = new RegExp(`(${pattern})`, 'gi'); // 'gi' for global and case-insensitive match
+
+    // Split the input string based on the pattern
+    const parts = input.split(regexp);
+
+    // Map the parts to an array of objects
+    return parts.map((part) => ({
+      text: part,
+      highlight: phrases.includes(part),
+    }));
+  }
+
+  let modifiedContent = highlightPhrases(infoContent, highlightedText ?? []);
+
   return (
     <View style={[styles.main, style]} testID={testId}>
       <Image style={styles.image} source={iconSrc} />
-      <SecondaryText style={styles.text} value={infoContent} />
+
+      <View>
+        <Text style={styles.text} testID={`${testId}-text`}>
+          {modifiedContent.map((v, k) =>
+            !v.highlight ? (
+              v.text
+            ) : (
+              <Text
+                key={k}
+                style={[styles.highlightedTextStyle, highlightedTextStyle]}
+              >
+                {v.text}
+              </Text>
+            )
+          )}
+        </Text>
+        {infoSubContent && (
+          <SecondaryText
+            style={styles.subText}
+            name={`${testId}-sub-text`}
+            value={infoSubContent}
+          />
+        )}
+      </View>
     </View>
   );
 };
@@ -34,16 +99,33 @@ const InformationIconText: React.FC<InformationIconProps> = ({
 const styles = StyleSheet.create({
   main: {
     flexDirection: 'row',
-    marginHorizontal: 24,
   },
   image: {
     width: 32,
     height: 32,
+    backgroundColor: 'gray',
   },
   text: {
     paddingStart: 8,
     paddingRight: 48,
     textAlign: 'left',
+    flexWrap: 'wrap',
+    fontWeight: '700',
+    fontSize: 18,
+    lineHeight: 25.2,
+  },
+  subText: {
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 19.6,
+    textAlign: 'left',
+    paddingStart: 8,
+    paddingRight: 48,
+    flexWrap: 'wrap',
+    color: '#4C627A',
+  },
+  highlightedTextStyle: {
+    color: 'red',
   },
 });
 
